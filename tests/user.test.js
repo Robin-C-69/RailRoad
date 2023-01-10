d5dbb40926dc2c57278f2f41860c65331b65c092
rorod <rorodin08082002@gmail.com>
2023-01-10 13:20:49 +0100

    Finished tests

--- tests/user.test.js
+++ tests/user.test.js
@@ -11,11 +11,27 @@ const objectUser = {
     "__v": Number
 }
 
+// Tests for user endpoint
 describe('User', () => {
-    it('GET /user', async () => {
+    it('Get all users', async () => {
         await supertest(app)
         .get('/user')
         .expect(200)
+        .then((response) => {
+            assert.equal(typeof(response.body), 'object')
+        })
+    })
+    it('Create user', async () => {
+        const userTest = {
+            pseudo: "testuser",
+            email: "test@test.fr",
+            password: "T3st",
+            role: "user"
+        }
+        await supertest(app)
+        .post('/user')
+        .send(userTest)
+        .expect(201)
         .then((response) => {
             assert.equal(typeof(response.body), 'object')
         })
